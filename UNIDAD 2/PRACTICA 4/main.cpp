1e4b466823677b2d0ad7c252749518f03a56139e
DiegoNighty <soydiegocardenas123@gmail.com>
2024-09-14 04:13:17 +0700

    aaaaaa

---
+++ UNIDAD 2/PRACTICA 4/main.cpp
@@ -0,0 +1,16 @@
+// PRACTICA 4 UNIDAD 2
+// Siguiente: informa el siguiente valor
+// Diego Cárdenas 12/09/2024
+#include <iostream>
+using namespace std;
+
+int main() {
+    int valor;
+    cout << "Ingresa el valor de la variable: " << endl;
+    cin >> valor;
+
+    int siguiente = valor + 1;
+    cout << "El siguiente valor es: " << siguiente << endl;
+
+    return 0;
+}
