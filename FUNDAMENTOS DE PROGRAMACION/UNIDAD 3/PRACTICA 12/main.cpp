d1519e7e3b5e89c8aa25f732e69d9d1daebd62c2
DiegoNighty <soydiegocardenas123@gmail.com>
2024-10-09 11:34:26 -0500

    practica 12

--- FUNDAMENTOS DE PROGRAMACION/UNIDAD 3/PRACTICA 12/main.cpp
+++ FUNDAMENTOS DE PROGRAMACION/UNIDAD 3/PRACTICA 12/main.cpp
@@ -8,6 +8,28 @@ int main() {
     cin >> letra;
 
     switch (letra) {
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'f':
+    case 'g':
+    case 'h':
+    case 'j':
+    case 'k':
+    case 'l':
+    case 'm':
+    case 'n':
+    case 'p':
+    case 'q':
+    case 'r':
+    case 's':
+    case 't':
+    case 'w':
+    case 'x':
+    case 'y':
+    case 'z':
+        cout << "Usted ingreso la consonante " << letra;
+        break;
     case 'a':
     case 'i':
     case 'e':
