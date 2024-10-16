f5169af02296479b4e21fe46b5c9700f88c126c8
DiegoNighty <soydiegocardenas123@gmail.com>
2024-10-16 11:41:20 -0500

    practicas

---
+++ FUNDAMENTOS DE PROGRAMACION/UNIDAD 4/PRACTICA 1/main.cpp
@@ -0,0 +1,10 @@
+// PRACTICA 1 UNIDAD 4
+// DIEGO CÁRDENAS
+#include <iostream>
+using namespace std;
+int main() {
+    for (int c = 1; c <= 5; c++) {
+        cout << "Sigo?" << endl;
+    }
+    return 0;
+}
---
+++ FUNDAMENTOS DE PROGRAMACION/UNIDAD 4/PRACTICA 2/main.cpp
@@ -0,0 +1,20 @@
+// PRACTICA 2 UNIDAD 4 DIEGO CARDENAS
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ingresa el numero de veces que se repite";
+    cin >> n;
+
+    for (int c = 1; c <= n; c++) {
+        cout << c << endl;
+    }
+
+    int c = 1;
+    while (c <= 10) {
+        cout << "hola";
+        c++;
+    }
+
+    return 0;
+}
