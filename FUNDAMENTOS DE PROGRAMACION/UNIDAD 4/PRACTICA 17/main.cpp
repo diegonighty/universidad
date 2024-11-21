dcd3af9299c4a7ab9b31532b94dc084923d5ce9b
DiegoNighty <soydiegocardenas123@gmail.com>
2024-11-21 11:31:44 -0600

    cp

--- FUNDAMENTOS DE PROGRAMACION/UNIDAD 4/PRACTICA 17/main.cpp
+++ FUNDAMENTOS DE PROGRAMACION/UNIDAD 4/PRACTICA 17/main.cpp
@@ -13,11 +13,30 @@ int main() {
         cout << "v[" << i << "] = ";
         cin >> v[i];
         cout << endl;
+
+        if (v[i] < 0 || v[i] > 100) {
+            cout << "El valor ingresado no es valido" << endl;
+            i--;
+        }
     }
 
+    int promedio = 0;
+    for (int i = 0; i < n; ++i) {
+        promedio += v[i];
+    }
+
+    promedio /= n;
+    cout << "El promedio es: " << promedio << endl;
+    int cp = 0;
     for (int i = 0; i < n; i++) {
         cout << "v[" << i << "] = " << v[i] << endl;
+
+        if (v[i] == promedio) {
+            cp++;
+        }
     }
+
+    cout << "El promedio se encontro " << cp << " veces" << endl;
 
     return 0;
 }
