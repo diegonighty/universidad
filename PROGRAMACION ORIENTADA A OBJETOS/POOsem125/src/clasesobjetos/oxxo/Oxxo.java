fd42c9a9fccf345ee640c0f852949275d288ab72
diegonighty <diego.cardenas.v06@gmail.com>
2025-03-27 11:26:09 -0500

    avances?

--- PROGRAMACION ORIENTADA A OBJETOS/POOsem125/src/clasesobjetos/oxxo/Oxxo.java
+++ PROGRAMACION ORIENTADA A OBJETOS/POOsem125/src/clasesobjetos/oxxo/Oxxo.java
@@ -2,7 +2,8 @@ package clasesobjetos.oxxo;
 
 public class Oxxo {
     public static void main(String[] args) {
-        Producto producto1;
-        producto1 = new Producto()
+        var producto1 = new Producto(10, "Sabritas", 50, 15, 500);
+        var cliente1 = new Cliente(1, "Juan", 1000);
+        cliente1.comprar(producto1);
     }
 }
